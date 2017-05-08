ab164340b7907e725f28db2bb4e3d8434fea5bc2
Afentoulis Konstantinos <kostasgoume@hotmail.com>
2017-05-08 23:47:48 +0300

    Update TwitterStreamGlobe.js

--- public/javascripts/TwitterStreamGlobe.js
+++ public/javascripts/TwitterStreamGlobe.js
@@ -163,7 +163,7 @@
 	 */ 
 	function render () {
 
-		earthMesh.rotation.y = earthMesh.rotation.y + 0.005;
+		earthMesh.rotation.y = earthMesh.rotation.y + 0.0025;
 		
 	  renderer.autoClear = false;
 	  renderer.clear();
